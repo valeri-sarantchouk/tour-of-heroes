8f8de032d79ec4fef5fa4e2401856776d80d54fa
Valeri <valeri.sarantchouk@gmail.com>
2017-09-04 22:49:46 -0400

    Added chapter 4 of Tour of Heroes

---
+++ src/app/component/hero-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Hero } from "./hero";
+
+@Component({
+  selector: 'hero-detail',
+  templateUrl: './hero.detail.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class HeroDetailComponent {
+  @Input() hero: Hero;
+}
---
+++ src/app/component/heroes.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+import {Hero} from "./hero";
+import { HeroService } from '../service/hero.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [ HeroService ]
+})
+
+//export class HeroesComponent {
+export class HeroesComponent implements OnInit {
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) {}
+
+  // constructor(private heroService: HeroService) {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    console.log("HeroesComponent selectedHero: " + JSON.stringify(hero));
+    this.selectedHero = hero;
+  }
+}
